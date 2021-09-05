c325736868235d0c22d345594d266e8830eb02ad
7204-OMER <Eyupbhr24@gmail.com>
2021-09-05 19:40:30 +0300

    add urls.py

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 from .views import (
     home,
-    postview,
-    postcreate,
-    postviewcreate,
-    postdetail
+    # postview,
+    # postcreate,
+    # postviewcreate,
+    # postdetail,
+    PostListCreateAPIView
 )
 
 urlpatterns = [
     path('', home),
-    path('postview/', postview),
-    path('postcreate/', postcreate),
-    path('postviewcreate/', postviewcreate),
-    path('postdetail/<int:pk>/', postdetail)
+    # path('postview/', postview),
+    # path('postcreate/', postcreate),
+    path('postviewcreate/', PostListCreateAPIView.as_view()),
+    # path('postdetail/<int:pk>/', postdetail),
+    # path('PostListCreateAPIView/<int:pk>/', PostListCreateAPIView)
 ]
