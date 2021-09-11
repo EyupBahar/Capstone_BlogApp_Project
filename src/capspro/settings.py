7dc96c452437c4b3c761417a2c9f9e0a58da27e7
7204-OMER <Eyupbhr24@gmail.com>
2021-09-12 00:51:18 +0300

    add settings.py

--- src/capspro/settings.py
+++ src/capspro/settings.py
@@ -44,11 +44,14 @@ INSTALLED_APPS = [
 
     #myapps
     'blog.apps.BlogConfig',
+    'user',
 
     #thirdparty
     'rest_framework',
     'django_filters',
     'corsheaders',
+    'rest_framework.authtoken',
+    
 ]
 
 MIDDLEWARE = [
@@ -132,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -144,8 +148,27 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
 
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = (
+# os.path.join(BASE_DIR, 'static'),
+# )
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+
+        # 'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+}
+
+# django_heroku.settings(locals())
 
 
 
