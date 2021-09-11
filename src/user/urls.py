dca5ecdacd9a614c687a4d450039c81590a3b733
7204-OMER <Eyupbhr24@gmail.com>
2021-09-12 00:51:42 +0300

    add urls.py

---
+++ src/user/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path
+from .views import CustomAuthToken, register_view, logout_view
+
+urlpatterns = [
+    path('login/', CustomAuthToken.as_view()),
+    path('register/', register_view),
+    path('logout/', logout_view),
+]
