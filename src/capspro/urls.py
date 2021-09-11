a8d1a5b209dd7b265c00550eda2641dedad89820
7204-OMER <Eyupbhr24@gmail.com>
2021-09-12 00:51:57 +0300

    add ursl.py

--- src/capspro/urls.py
+++ src/capspro/urls.py
@@ -15,14 +15,16 @@ Including another URLconf
 """
 
 
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))    
+    path('', include('blog.urls')),    
+    path('user/', include('user.urls')),
+    path('api-auth/', include('rest_framework.urls')),   
 ]
 
 
