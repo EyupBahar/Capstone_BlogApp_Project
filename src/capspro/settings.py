bf1c6518ac28efc8784d9cb7d116c2944be53b80
7204-OMER <Eyupbhr24@gmail.com>
2021-09-15 14:01:41 +0300

    add setting.py updates

--- src/capspro/settings.py
+++ src/capspro/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
