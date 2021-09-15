ffff3810deff86a5125c2a519536d04d4419b7d3
7204-OMER <Eyupbhr24@gmail.com>
2021-09-15 13:25:49 +0300

    add last changes

--- src/blog/models.py
+++ src/blog/models.py
@@ -32,7 +32,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment")
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
