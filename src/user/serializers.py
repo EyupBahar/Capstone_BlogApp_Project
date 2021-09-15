4610796580c11ac58ab7b91e0c0431b42c038d37
7204-OMER <Eyupbhr24@gmail.com>
2021-09-15 19:17:49 +0300

    add serializer.py

--- src/user/serializers.py
+++ src/user/serializers.py
@@ -7,3 +7,8 @@ class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+    
+    def create(self, validated_data):
+        user= User.objects.create(username=validated_data['username'],email = validated_data['email'])
+        user.set_password(validated_data['password'])
+        user.save()
