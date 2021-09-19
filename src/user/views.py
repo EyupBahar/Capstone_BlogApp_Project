d66d82037e2597f3fa031cf5077772e0da76ece9
7204-OMER <Eyupbhr24@gmail.com>
2021-09-19 22:55:35 +0300

    add views.py

--- src/user/views.py
+++ src/user/views.py
@@ -30,14 +30,14 @@ def register_view(request):
     #     user.save()
 
 class CustomAuthToken(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
 
+    def post(self, request, *args, **kwargs):
         print(request.data)
         serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
-    
+
         return Response({
             'token': token.key,
             'user_id': user.pk, 
